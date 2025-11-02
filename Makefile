df7c15e0c66b5ec1ea61f187f09e0781b846f388
Carl-Christian Sautter <mail@c-sautter.de>
2025-11-02 10:40:01 +0100

    test: added all ubuntu build integration test cases on macos

---
+++ Makefile
@@ -0,0 +1,2 @@
+test-build:
+	go test -timeout 300m -v ./pkg/build/*_test.go
--- pkg/build/macos-silicon_test.go
+++ pkg/build/macos-silicon_test.go
@@ -2,21 +2,32 @@ package build
 
 import (
 	"bufio"
+	"context"
 	"os/exec"
 	"runtime"
 	"testing"
+	"time"
 )
 
-
+type VirtualMachineConfig struct {
+	OS         string
+	Arch       string
+	UbuntuType string
+}
 
 func TestPrintSystemOsArch(t *testing.T) {
 	t.Logf("Running on OS: %s, ARCH: %s", runtime.GOOS, runtime.GOARCH)
 }
 
-func TestRunLinuxUbuntuOnMacOS(t *testing.T) {
+func RunQemuUbuntuBuildOnMacOS(t *testing.T, config VirtualMachineConfig) {
 	scriptPath := "../../build/packer/linux/ubuntu/linux-ubuntu-on-macos.sh"
-	args := []string{"--arch", "arm64", "--ubuntu-type", "server"}
-	cmd := exec.Command("bash", append([]string{scriptPath}, args...)...)
+	args := []string{"--arch", config.Arch, "--ubuntu-type", config.UbuntuType}
+
+	// Set a timeout for the script execution (adjust as needed)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "bash", append([]string{scriptPath}, args...)...)
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -30,6 +41,8 @@ func TestRunLinuxUbuntuOnMacOS(t *testing.T) {
 	if err := cmd.Start(); err != nil {
 		t.Fatalf("Failed to start command: %v", err)
 	}
+
+	done := make(chan error, 1)
 
 	go func() {
 		scanner := bufio.NewScanner(stdout)
@@ -45,9 +58,55 @@ func TestRunLinuxUbuntuOnMacOS(t *testing.T) {
 		}
 	}()
 
-	err = cmd.Wait()
-	if err != nil {
-		t.Fatalf("Script failed: %v", err)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case err := <-done:
+		if err != nil {
+			t.Fatalf("Script failed: %v", err)
+		}
+		t.Logf("Script finished successfully.")
+	case <-ctx.Done():
+		// Kill the process if context is done (timeout or cancellation)
+		_ = cmd.Process.Kill()
+		t.Fatalf("Script terminated due to timeout or interruption: %v", ctx.Err())
 	}
-	t.Logf("Script finished successfully.")
+}
+
+func TestBuildQemuUbuntuServerArm64OnMacos(t *testing.T) {
+	VirtualMachineConfig := VirtualMachineConfig{
+		OS:         "debian",
+		Arch:       "arm64",
+		UbuntuType: "server",
+	}
+	RunQemuUbuntuBuildOnMacOS(t, VirtualMachineConfig)
+}
+
+func TestBuildQemuUbuntuServerAmd64OnMacos(t *testing.T) {
+	VirtualMachineConfig := VirtualMachineConfig{
+		OS:         "debian",
+		Arch:       "amd64",
+		UbuntuType: "server",
+	}
+	RunQemuUbuntuBuildOnMacOS(t, VirtualMachineConfig)
+}
+
+func TestBuildQemuUbuntuDesktopArm64OnMacos(t *testing.T) {
+	VirtualMachineConfig := VirtualMachineConfig{
+		OS:         "debian",
+		Arch:       "arm64",
+		UbuntuType: "desktop",
+	}
+	RunQemuUbuntuBuildOnMacOS(t, VirtualMachineConfig)
+}
+
+func TestBuildQemuUbuntuDesktopAmd64OnMacos(t *testing.T) {
+	VirtualMachineConfig := VirtualMachineConfig{
+		OS:         "debian",
+		Arch:       "amd64",
+		UbuntuType: "desktop",
+	}
+	RunQemuUbuntuBuildOnMacOS(t, VirtualMachineConfig)
 }
