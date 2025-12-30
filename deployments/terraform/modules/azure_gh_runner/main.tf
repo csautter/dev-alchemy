b435dbdad542a07aada27dcee341f4c3c12b602a
Carl-Christian Sautter <mail@c-sautter.de>
2025-12-30 14:29:40 +0100

    feat: created terraform/terragrunt base structure
    * tf module for state backend creation
    * skeleton for gh runner module

---
+++ deployments/terraform/modules/azure_gh_runner/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "this" {
+  name     = "azure-github-runner-rg-${var.env}"
+  location = data.azurerm_location.east_us.location
+  tags = {
+    shortname = "azure-github-runner"
+    env       = var.env
+  }
+}
+
+data "azurerm_location" "east_us" {
+  location = "East US"
+}
