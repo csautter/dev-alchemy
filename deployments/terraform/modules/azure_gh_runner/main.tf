ca9ed649bac251881a1d4d7e1564f9118388869a
Carl-Christian Sautter <mail@c-sautter.de>
2026-01-04 19:26:21 +0100

    feat: terraform code for function app

--- deployments/terraform/modules/azure_gh_runner/main.tf
+++ deployments/terraform/modules/azure_gh_runner/main.tf
@@ -1,12 +1,3 @@
-resource "azurerm_resource_group" "this" {
-  name     = "azure-github-runner-rg-${var.env}"
-  location = data.azurerm_location.east_us.location
-  tags = {
-    shortname = "azure-github-runner"
-    env       = var.env
-  }
-}
-
 data "azurerm_location" "east_us" {
   location = "East US"
 }
