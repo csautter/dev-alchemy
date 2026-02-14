cfccd4305350011b6c4758137c9ba53a24eb023e
Carl-Christian Sautter <mail@c-sautter.de>
2026-02-14 17:20:21 +0100

    feat: gh runner vm set default switch

--- scripts/gh-runner-func/function_app.py
+++ scripts/gh-runner-func/function_app.py
@@ -24,6 +24,50 @@ Add-Content -Path "C:\execution.log" -Value "cloud-init-combined.ps1 executed on
 if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).State -ne 'Enabled') {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Clients -All -NoRestart
+    
+    # create default virtual switch with NAT network (required for nested virtualization)
+    if (-not (Get-VMSwitch -Name "Default Switch" -ErrorAction SilentlyContinue)) {
+        New-VMSwitch -Name "Default Switch" -SwitchType Internal
+        $netAdapter = Get-NetAdapter | Where-Object { $_.Name -like "vEthernet (Default Switch)" }
+        if ($netAdapter) {
+            New-NetIPAddress -IPAddress "192.168.0.1" -PrefixLength 24 -InterfaceIndex $netAdapter.ifIndex
+            New-NetNat -Name "DefaultSwitchNAT" -InternalIPInterfaceAddressPrefix "192.168.0.0/24"
+        }
+    }
+
+    # After creating the NAT, add DHCP Server
+    if (-not (Get-WindowsFeature -Name DHCP -ErrorAction SilentlyContinue).Installed) {
+        Install-WindowsFeature -Name DHCP -IncludeManagementTools
+    }
+
+    # Configure DHCP scope
+    $scopeName = "DefaultSwitchDHCP"
+    if (-not (Get-DhcpServerv4Scope -ScopeId 192.168.0.0 -ErrorAction SilentlyContinue)) {
+        Add-DhcpServerv4Scope -Name $scopeName `
+            -StartRange 192.168.0.100 `
+            -EndRange 192.168.0.200 `
+            -SubnetMask 255.255.255.0 `
+            -State Active
+        
+        Set-DhcpServerv4OptionValue -ScopeId 192.168.0.0 `
+            -Router 192.168.0.1 `
+            -DnsServer 8.8.8.8,8.8.4.4
+    }
+
+    # Bind DHCP server only to the Default Switch adapter
+    $switchAdapter = Get-NetAdapter | Where-Object { $_.Name -like "vEthernet (Default Switch)" }
+    if ($switchAdapter) {
+        Set-DhcpServerv4Binding -InterfaceAlias $switchAdapter.Name -BindingState $true
+        
+        # Unbind from all other interfaces to avoid conflicts with Azure VNet DHCP
+        Get-DhcpServerv4Binding | Where-Object { $_.InterfaceAlias -ne $switchAdapter.Name } | 
+            ForEach-Object { Set-DhcpServerv4Binding -InterfaceAlias $_.InterfaceAlias -BindingState $false }
+    }
+
+    # Authorize DHCP server (required for domain-joined machines)
+    Add-DhcpServerInDC -DnsName $env:COMPUTERNAME
+
     # Restart to complete Hyper-V installation
     Write-Host 'Restart required to complete Hyper-V installation.'
     Restart-Computer -Force
