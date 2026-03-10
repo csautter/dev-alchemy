7724f4a91bad9cda12bd89883fec822077a52f3e
Carl-Christian Sautter <mail@c-sautter.de>
2026-03-10 20:57:58 +0100

    fix(deploy): prevent truncated streamed logs and surface scanner errors

--- pkg/deploy/command-runner.go
+++ pkg/deploy/command-runner.go
@@ -4,9 +4,16 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"sync"
 	"time"
+)
+
+const (
+	scannerInitialBufferSize = 64 * 1024
+	scannerMaxBufferSize     = 1024 * 1024
 )
 
 func runCommandWithStreamingLogs(workingDir string, timeout time.Duration, executable string, args []string, logPrefix string) {
@@ -30,20 +37,23 @@ func runCommandWithStreamingLogs(workingDir string, timeout time.Duration, execu
 	}
 
 	done := make(chan error, 1)
+	var streamsWG sync.WaitGroup
 
-	go func() {
-		scanner := bufio.NewScanner(stdout)
+	streamOutput := func(output io.Reader, streamName string) {
+		defer streamsWG.Done()
+		scanner := bufio.NewScanner(output)
+		scanner.Buffer(make([]byte, scannerInitialBufferSize), scannerMaxBufferSize)
 		for scanner.Scan() {
-			log.Printf("%s stdout: %s", logPrefix, scanner.Text())
+			log.Printf("%s %s: %s", logPrefix, streamName, scanner.Text())
 		}
-	}()
+		if err := scanner.Err(); err != nil {
+			log.Printf("%s %s scanner error: %v", logPrefix, streamName, err)
+		}
+	}
 
-	go func() {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			log.Printf("%s stderr: %s", logPrefix, scanner.Text())
-		}
-	}()
+	streamsWG.Add(2)
+	go streamOutput(stdout, "stdout")
+	go streamOutput(stderr, "stderr")
 
 	go func() {
 		done <- cmd.Wait()
@@ -51,12 +61,14 @@ func runCommandWithStreamingLogs(workingDir string, timeout time.Duration, execu
 
 	select {
 	case err := <-done:
+		streamsWG.Wait()
 		if err != nil {
 			panic(fmt.Sprintf("Command failed (%s %v): %s", executable, args, err.Error()))
 		}
 		log.Printf("Command finished successfully: %s %v", executable, args)
 	case <-ctx.Done():
 		_ = cmd.Process.Kill()
+		streamsWG.Wait()
 		panic("Command terminated due to timeout or interruption: " + ctx.Err().Error())
 	}
 }
